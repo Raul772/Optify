d45c55f4d5376d937982d0936003aa758e0dad29
Raul Victor <31050006+Raul772@users.noreply.github.com>
2025-05-01 02:08:22 -0300

    feat(notificação): adicionar ícones personalizados para lembretes da regra 20-20-20 chore(PyInstaller): atualizar arquivo .spec com configurações de otimização chore: atualizar dependências e configuração

--- Optify/main.py
+++ Optify/main.py
@@ -1,8 +1,17 @@
+import sys
+import os
 import time
 from plyer import notification
 from PIL import Image
 from pystray import Menu, MenuItem, Icon
 from threading import Thread
+
+
+def get_icon_path():
+    if getattr(sys, 'frozen', False):
+        return os.path.join(sys._MEIPASS, "icon.ico")
+    else:
+        return "icon.ico"
 
 
 def notify_202020_rule():
@@ -12,6 +21,7 @@ def notify_202020_rule():
             title="Regra 20-20-20!",
             message="Faça uma pausa! Olhe para algo a 20 pés (~6 metros) por 20 segundos.",
             app_name="Optify",
+            app_icon=get_icon_path(),
             timeout=10,
         )
 
@@ -22,16 +32,18 @@ def notify_hourly_pause():
             title="Hora de uma pausa!",
             message="Levante-se e mova-se por 5 minutos.",
             app_name="Optify",
+            app_icon=get_icon_path(),
             timeout=10,
         )
 
 def notify_water_reminder():
     while True:
-        time.sleep(60 * 60)  # Lembrete a cada hora
+        time.sleep(60 * 60) 
         notification.notify(
             title="Hidrate-se!",
             message="Beba um copo d'água.",
             app_name="Optify",
+            app_icon=get_icon_path(),
             timeout=10,
         )
 
@@ -39,12 +51,10 @@ def quit_app(icon, item):
     icon.stop()
 
 def setup_tray():
-    image = Image.open("icon.ico")
+    image = Image.open(get_icon_path())
     menu = Menu(MenuItem("Sair", quit_app))
     icon = Icon("Optify", image, "Optify", menu)
     icon.run()
-
-    exit()
 
 def main():
    
